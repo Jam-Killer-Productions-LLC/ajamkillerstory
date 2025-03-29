0ebd4da7fafb91ed96f1a5b5cca41c59ce0b6357
Fletcher Christian <fletcher@jamkillerproductions.io>
2025-03-28 21:25:49 -0500

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { createThirdwebClient, getContract } from "thirdweb";
+import { optimism } from "thirdweb/chains";
 
-// This is your client ID from the thirdweb dashboard
+// ThirdWeb client ID
 const clientId = "e24d90c806dc62cef0745af3ddd76314";
 
+// Create the thirdweb client using your clientId
+const client = createThirdwebClient({
+  clientId: clientId,
+});
+
+// Connect to your NFT contract on Optimism
+const contract = getContract({
+  client,
+  chain: optimism,
+  address: "0xfA2A3452D86A9447e361205DFf29B1DD441f1821",
+});
+
+// Use the already created client in ThirdwebProvider
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThirdwebProvider 
       clientId={clientId}
       activeChain="optimism"
     >
-      <App contract={null} />
+      <App contract={contract} />
     </ThirdwebProvider>
   </React.StrictMode>
 );
